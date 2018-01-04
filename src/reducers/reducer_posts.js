349398a04e8d5524b62cb87b7b01147342fcd725
Mahsa Mohammadi <mohammadi1@outlook.com>
2018-01-05 03:03:53 +1100

    Deleting Posts Improved

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,11 +1,12 @@
 // we don't need to say file name because it is index.js
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
-import { FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case DELETE_POST:
+      return _.omit(state, action.payload);
     case FETCH_POST:
       /** ES5 Syntax:
       const post = action.payload.data;
