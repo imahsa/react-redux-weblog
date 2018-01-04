40530e1eec51ee173d441d80286489539d3ea056
Mahsa Mohammadi <mohammadi1@outlook.com>
2018-01-04 22:10:57 +1100

    Form CSS Styles improved

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -11,14 +11,23 @@ import { Field, reduxForm } from 'redux-form';
  */
 class PostsNew extends Component {
   renderField(field) {
+    // ES6 Destructering
+    const { meta: {touched, error} } = field;
+    // Un Destructered version
+    // const className = `form-group ${field.meta.touched && field.meta.error ? 'has-danger' : ''}`
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input className="form-control"
           type="text"
           {...field.input}
          />
-         {field.meta.touched? field.meta.error : ''}
+         <div className="text-help">
+          {touched ? error : ''}
+         </div>
+
       </div>
     );
   }
